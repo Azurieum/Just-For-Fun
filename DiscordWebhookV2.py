1150bc60ec1d14d26f4e4fc8ead487d9d4fd12b9
Matthew <78315885+Azurieum@users.noreply.github.com>
2022-01-26 18:21:04 -0500

    Add files via upload

---
+++ DiscordWebhookV2.py
@@ -0,0 +1,25 @@
+from dhooks import Webhook, Embed
+import time
+TheWebHook = Webhook("https://discord.com/api/webhooks/935720711295754251/SwheYQcXCIW2U-KfAuw-LvFm-WcggMSsuloGYniAmDbVoaIDdqY20Mgcz1_F6YRJWS9c")
+neco = "https://media.discordapp.net/attachments/759572977770692627/935722923191324682/copim.jpeg"
+IMGorMSG = int(input("Message[1], Image[2] choose: "))
+#credit to dhook team helped me a lot! 
+#dhook github https://github.com/kyb3r/dhooks
+#Version 2 (V3 WHEN?)
+if IMGorMSG == 1 :
+    embed = Embed(
+    description = ("dont click random links"),
+    color = 0xff0000,
+    timestamp = 'now' 
+    )
+    embed.add_field(name = 'Message:', value = input("Say yo bullshit: ")) 
+    TheWebHook.send(embed = embed)
+elif IMGorMSG == 2: 
+    UserInputIMG = input("COPY AND PASTE YOUR PHOTO AS A DISCORD HOSTED LINK: ")
+    embed = Embed(
+    description = 'Image: ',
+    timestamp = 'now' 
+    )
+    embed.set_image(UserInputIMG)
+    TheWebHook.send(embed=embed)
+time.sleep(3)
